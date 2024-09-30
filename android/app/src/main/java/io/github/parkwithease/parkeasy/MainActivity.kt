cf93acbd5a48e0e68166590032ac547cbbf08fc5
edwinduong <edwinduong23@gmail.com>
2024-09-29 19:51:58 -0500

    android: update LoginPage to better follow UDF

--- android/app/src/main/java/io/github/parkwithease/parkeasy/MainActivity.kt
+++ android/app/src/main/java/io/github/parkwithease/parkeasy/MainActivity.kt
@@ -19,8 +19,10 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -31,7 +33,9 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import io.github.parkwithease.parkeasy.HttpService.login
 import io.github.parkwithease.parkeasy.ui.theme.ParkEasyTheme
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,7 +43,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ParkEasyTheme {
-                LoginForm()
+                LoginPage()
             }
         }
     }
@@ -47,14 +51,14 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showSystemUi = true, device = "id:pixel_8")
 @Composable
-fun LoginFormPreview() {
+fun LoginPagePreview() {
     ParkEasyTheme {
-        LoginForm()
+        LoginPage()
     }
 }
 
 @Composable
-fun LoginForm() {
+fun LoginPage() {
     Surface {
         Column {
             Row(
@@ -75,37 +79,62 @@ fun LoginForm() {
                     .weight(1f)
                     .fillMaxSize()
             ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier
-                        .fillMaxSize()
-                ) {
-                    EmailField()
-                    Spacer(Modifier.size(4.dp))
-                    PasswordField()
-                    Spacer(Modifier.size(4.dp))
-                    Row(
-                        modifier = Modifier.width(280.dp)
-                    ) {
-                        Spacer(Modifier.weight(1f)) // Space for future register button
-                        Spacer(Modifier.size(4.dp))
-                        LoginButton(
-                            modifier = Modifier.weight(1f)
-                        )
-                    }
-                }
+                LoginForm()
             }
         }
     }
 }
 
 @Composable
-fun EmailField() {
-    val email = remember { mutableStateOf("") }
+fun LoginForm() {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val onEmailChange = { input: String ->
+        email = input
+    }
+    val onPasswordChange = { input: String ->
+        password = input
+    }
+    val onLoginClick = { }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        EmailField(
+            email,
+            onEmailChange
+        )
+        Spacer(Modifier.size(4.dp))
+        PasswordField(
+            password,
+            onPasswordChange
+        )
+        Spacer(Modifier.size(4.dp))
+        Row(
+            modifier = Modifier.width(280.dp)
+        ) {
+            Spacer(Modifier.weight(1f)) // Space for future register button
+            Spacer(Modifier.size(4.dp))
 
+            LoginButton(
+                modifier = Modifier.weight(1f),
+                onLoginClick
+            )
+        }
+    }
+}
+
+@Composable
+fun EmailField(
+    text: String,
+    onValueChange: (String) -> Unit,
+) {
     OutlinedTextField(
-        value = email.value,
-        onValueChange = {email.value = it},
+        value = text,
+        onValueChange = {
+            onValueChange(it)
+        },
         label = {
             Text(stringResource(R.string.email))
         },
@@ -120,12 +149,15 @@ fun EmailField() {
 }
 
 @Composable
-fun PasswordField() {
-    val password = remember { mutableStateOf("") }
-
+fun PasswordField(
+    text: String,
+    onValueChange: (String) -> Unit
+) {
     OutlinedTextField(
-        value = password.value,
-        onValueChange = {password.value = it},
+        value = text,
+        onValueChange = {
+            onValueChange(it)
+        },
         label = {
             Text(stringResource(R.string.password))
         },
@@ -141,9 +173,12 @@ fun PasswordField() {
 }
 
 @Composable
-fun LoginButton(modifier: Modifier = Modifier) {
+fun LoginButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+    ) {
     Button(
-        onClick = { },
+        onClick = { onClick() },
         modifier = modifier
     ) {
         Text(stringResource(R.string.login))
