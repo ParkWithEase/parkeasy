93a3da0fb60d9b678e9f389bc2e23d65d2ec8dba
mayallhs <mayallh10@gmail.com>
2024-09-30 12:13:25 -0500

    Added new components and styled the app better

--- webapp/src/routes/+layout.svelte
+++ webapp/src/routes/+layout.svelte
@@ -1,31 +1,55 @@
-<script>import '../app.css';</script>
+<script>
+    import "../app.css";
+    import {
+        Footer,
+        FooterBrand,
+        FooterCopyright,
+        FooterIcon,
+        FooterLink,
+        FooterLinkGroup,
+    } from "flowbite-svelte";
+</script>
 
-<div class="app">
-    <main>
+<body class="app">
+    <main class="mainLoginPage">
         <slot></slot>
     </main>
-</div>
+</body>
 
+<Footer>
+    <div class="footer">
+        <FooterCopyright href="/" by="parkeasy™" year={2024} />
+        <FooterLinkGroup
+            ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
+        ></FooterLinkGroup>
+    </div>
+</Footer>
 
 <style>
+    @import url("https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap");
     .app {
         display: flex;
         flex-direction: column;
         align-items: center;
-        min-height: 100vh;
-        font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
         color: black;
-        background-image: url('../lib/images/background.jpg');
+        font-family: Fredoka, sans-serif;
+        background-image: url("$lib/images/background.png");
+        background-size: 100%;
     }
 
-    main {
-        flex: 1;
+    .mainLoginPage {
         display: flex;
-        flex-direction: column;
-        padding: 1rem;
-        width: 100%;
-        max-width: 64rem;
-        margin: 0 auto;
+        flex-direction: row;
         box-sizing: border-box;
+    }
+
+    .footer {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        color: black;
+        font-family: Fredoka, sans-serif;
+        background-color: rgb(7, 7, 7);
+        padding: 5px;
     }
 </style>
--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -1,13 +1,15 @@
 <script>
-    import logo from '$lib/images/parkeasy-logo.png';
-    import user from '../user';
-    import LoginPage from '$lib/LoginPage.svelte';
+    import logo from "$lib/images/parkeasy-logo.png";
+    import user from "../user";
+    import LoginPage from "$lib/LoginPage.svelte";
+    import { Input, Label, Helper, Button } from "flowbite-svelte";
+    import { UserCircleOutline, EnvelopeSolid } from "flowbite-svelte-icons";
 
-    $: isLoggedIn = $user === null? false: true;
+    $: isLoggedIn = $user === null ? false : true;
 
-    const logout = () =>{
-        user.update(val => val = null);
-    }
+    const logout = () => {
+        user.update((val) => (val = null));
+    };
 </script>
 
 <svelte:head>
@@ -15,20 +17,20 @@
 </svelte:head>
 
 <section class="loginForm">
-    <img src={logo} alt="logo" class="logo"/>
+    <img src={logo} alt="logo" class="logo" />
     {#if isLoggedIn}
-    <h2>Welcome back!</h2>
-    <input type="button" value="logout" on:click={logout}>
+        <h2>Welcome back!</h2>
+        <input type="button" value="logout" on:click={logout} />
     {:else}
-    <h2>Login</h2>
-    <LoginPage />
+        <h2>Login</h2>
+        <LoginPage />
     {/if}
 </section>
 
 <style>
-    h2{
+    h2 {
         font-size: 25px;
-        font-weight: bold ;
+        font-weight: bold;
     }
 
     .loginForm {
@@ -36,17 +38,16 @@
         flex-direction: column;
         justify-content: center;
         align-items: center;
-        margin: 6rem;
+        margin: 5rem 2rem 5rem;
+        height: 100%;
         padding: 2rem;
-        background-color: #95b18f;
+        background-color: #c3e9bb;
         border-radius: 2rem;
         border: 0.2rem solid #070707;
     }
 
-    .logo{
+    .logo {
         border-radius: 2rem;
         width: 20rem;
     }
-
-    
 </style>
