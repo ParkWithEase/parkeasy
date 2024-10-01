afa2eebf3865cdd604e5a26756937fcb602bb243
mayallhs <mayallh10@gmail.com>
2024-10-01 11:55:39 -0500

    Added files

---
+++ webapp/tests/loginPage.test.ts
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/svelte';
+import { expect, test, vi, describe } from 'vitest';
+import LoginPage from "../src/lib/LoginPage.svelte";
+
+const mockFetch = vi.fn();
+global.fetch = mockFetch;
+
+describe('render test', () => {
+    test('all components should render correctly', async () => {
+        mockFetch.mockResolvedValueOnce({
+            ok: true,
+            json: () => {}
+        });
+
+        render(LoginPage);
+        const newPassword = screen.getByLabelText('Username');
+        expect(newPassword).toBeDefined();
+        const confirmPassword = screen.getByText('Password');
+        expect(confirmPassword).toBeDefined();
+
+        const button = screen.getByText('Submit');
+        expect(button).toBeDefined();
+    });
+}
