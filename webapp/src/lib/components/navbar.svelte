3b76373ad9663a895c3fba1e56ab2f9f8c0639e4
mayallhs <mayallh10@gmail.com>
2024-10-05 00:25:00 -0500

    Added a navbar component

---
+++ webapp/src/lib/components/navbar.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+    import logo from '$lib/images/logo-text.png';
+    </script>
+    
+    <svelte:window/>
+    <nav class="navbar">
+        <a href="/#"
+            ><img src={logo} alt="parkeasy-logo" class="text-logo"
+        /></a>
+        <a class="nav-link" href="/profile/user-profile">book a spot</a>
+        <a class="nav-link" href="/profile/user-profile">list your spot</a>
+        <a class="nav-link" href="/">about us</a>
+    </nav>
+    
+    <style> 
+    
+        .navbar {   
+            position: fixed;
+            top: 0;
+            width: 100%;  
+            height: 7rem; 
+            background-color: #000;   
+            color: #FFF;   
+            z-index: -1;  
+            display: flex;   
+            flex-direction: row;
+            justify-content: space-evenly; 
+            align-items: center; 
+            transition: all 0.2s ease-out;
+        } 
+    
+        .nav-link {
+            background-color: white;
+            padding: 1rem 2rem;
+            border-radius: 20px;
+            font-size: 1rem;
+            font-weight: bold;
+            color: black;
+            text-decoration: none;
+        }
+
+        .nav-link:hover{
+            color: #7ed957;
+            background-color: rgb(54, 54, 54);
+            transition: 0.3s;
+        }
+
+    
+        .text-logo{
+            max-height: 7rem;
+        }
+        
+          
+    </style> 
