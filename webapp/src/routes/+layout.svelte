17b4e3fb70b242f6f71960f1c28bdcf2814e4d70
mayallhs <mayallh10@gmail.com>
2024-10-04 20:55:09 -0500

    Added styling for fonts

--- webapp/src/routes/+layout.svelte
+++ webapp/src/routes/+layout.svelte
@@ -5,22 +5,13 @@
 <slot></slot>
 
 <footer class="footer">
-    <p>By ParkEasy Team 2024</p>
+    <p>&copyparkeasy 2024</p>
 </footer>
 
 <style>
-    /* @import url('https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap');
-    .app {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        color: black;
-        font-family: Fredoka, sans-serif;
-        background-image: url('$lib/images/background.png');
-        background-size: 100%;
-    } */
-
     .footer {
+        background-color: black;
+        color: lightgreen;
         position: absolute;
         text-align: center;
         width: 100%;
