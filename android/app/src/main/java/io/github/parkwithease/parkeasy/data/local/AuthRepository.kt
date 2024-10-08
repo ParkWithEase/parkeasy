b446d0e18bd4a38280a31f8258512c3a75e0e0f3
edwinduong <edwinduong23@gmail.com>
2024-10-07 23:20:58 -0500

    android: add KDoc comments to AuthRepository, UserRepository

--- android/app/src/main/java/io/github/parkwithease/parkeasy/data/local/AuthRepository.kt
+++ android/app/src/main/java/io/github/parkwithease/parkeasy/data/local/AuthRepository.kt
@@ -3,11 +3,29 @@ package io.github.parkwithease.parkeasy.data.local
 import io.ktor.http.Cookie
 
 interface AuthRepository {
+    /**
+     * Gets the session Cookie.
+     *
+     * @return the session Cookie.
+     */
     suspend fun getSession(): Cookie?
 
+    /**
+     * Gets the session status.
+     *
+     * @return true if user logged in, false otherwise.
+     */
     suspend fun getStatus(): Boolean
 
+    /**
+     * Sets the session Cookie and status to logged in.
+     *
+     * @param cookie the session Cookie to set.
+     */
     suspend fun set(cookie: Cookie)
 
+    /**
+     * Clears the session Cookie and sets session status to logged out.
+     */
     suspend fun reset()
 }
