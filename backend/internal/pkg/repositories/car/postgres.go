724850c918cbf53e72fb30bd3b981becf3990be9
Hiếu Lê <181802858+ioreel@users.noreply.github.com>
2024-10-08 00:19:31 -0500

    backend/repo/car: add postgres implementation for car DB

---
+++ backend/internal/pkg/repositories/car/postgres.go
@@ -0,0 +1,139 @@
+package car
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"math"
+
+	"github.com/ParkWithEase/parkeasy/backend/internal/pkg/dbmodels"
+	"github.com/ParkWithEase/parkeasy/backend/internal/pkg/models"
+	"github.com/aarondl/opt/omit"
+	"github.com/google/uuid"
+	"github.com/stephenafamo/bob"
+	"github.com/stephenafamo/bob/dialect/psql"
+	"github.com/stephenafamo/bob/dialect/psql/dm"
+	"github.com/stephenafamo/bob/dialect/psql/sm"
+	"github.com/stephenafamo/bob/dialect/psql/um"
+	"github.com/stephenafamo/scan"
+)
+
+type PostgresRepository struct {
+	db bob.DB
+}
+
+func NewPostgres(db bob.DB) *PostgresRepository {
+	return &PostgresRepository{
+		db: db,
+	}
+}
+
+func (p *PostgresRepository) Create(ctx context.Context, userID64 int64, car *models.CarCreationInput) (uuid.UUID, error) {
+	if userID64 > math.MaxInt32 || userID64 < 0 {
+		return uuid.Nil, fmt.Errorf("userID out of range: %v", userID64)
+	}
+	userID := int32(userID64)
+
+	inserted, err := dbmodels.Cars.Insert(ctx, p.db, &dbmodels.CarSetter{
+		Userid:       omit.From(userID),
+		Licenseplate: omit.From(car.LicensePlate),
+		Make:         omit.From(car.Make),
+		Model:        omit.From(car.Model),
+		Color:        omit.From(car.Color),
+	})
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("could not commit transaction: %w", err)
+	}
+	return inserted.Caruuid, nil
+}
+
+func (p *PostgresRepository) DeleteByUUID(ctx context.Context, carID uuid.UUID) error {
+	query := psql.Delete(
+		dm.From(dbmodels.Cars.Name(ctx)),
+		dbmodels.DeleteWhere.Cars.Caruuid.EQ(carID),
+	)
+
+	// Execute the query
+	_, err := bob.Exec(ctx, p.db, query)
+	if err != nil {
+		return fmt.Errorf("could not execute delete: %w", err)
+	}
+
+	return nil
+}
+
+func (p *PostgresRepository) UpdateByUUID(ctx context.Context, carID uuid.UUID, car *models.CarCreationInput) (Entry, error) {
+	query := psql.Update(
+		um.From(dbmodels.Cars.Name(ctx)),
+		dbmodels.UpdateWhere.Cars.Caruuid.EQ(carID),
+		um.Set(dbmodels.CarColumns.Licenseplate, psql.Arg(car.LicensePlate)),
+		um.Set(dbmodels.CarColumns.Make, psql.Arg(car.Make)),
+		um.Set(dbmodels.CarColumns.Model, psql.Arg(car.Model)),
+		um.Set(dbmodels.CarColumns.Color, psql.Arg(car.Color)),
+	)
+
+	// Execute the query
+	_, err := bob.Exec(ctx, p.db, query)
+	if err != nil {
+		return Entry{}, fmt.Errorf("could not execute update: %w", err)
+	}
+
+	return Entry{}, nil
+}
+
+func (p *PostgresRepository) GetByUUID(ctx context.Context, carID uuid.UUID) (Entry, error) {
+	query := psql.Select(
+		sm.Columns(
+			dbmodels.CarColumns.Licenseplate,
+			dbmodels.CarColumns.Make,
+			dbmodels.CarColumns.Model,
+			dbmodels.CarColumns.Color,
+			dbmodels.CarColumns.Carid,
+			dbmodels.CarColumns.Userid,
+		),
+		sm.From(dbmodels.Cars.Name(ctx)),
+		dbmodels.SelectWhere.Cars.Caruuid.EQ(carID),
+	)
+	result, err := bob.One(ctx, p.db, query, scan.StructMapper[dbmodels.Car]())
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			err = ErrNotFound
+		}
+		return Entry{}, err
+	}
+
+	details := models.CarDetails{
+		LicensePlate: result.Licenseplate,
+		Make:         result.Make,
+		Model:        result.Model,
+		Color:        result.Color,
+	}
+
+	car := models.Car{
+		Details: details,
+	}
+
+	return Entry{
+		Car:        car,
+		InternalID: int64(result.Carid),
+		OwnerID:    int64(result.Userid),
+	}, err
+}
+
+func (p *PostgresRepository) GetOwnerByUUID(ctx context.Context, carID uuid.UUID) (int64, error) {
+	query := psql.Select(
+		sm.Columns(dbmodels.CarColumns.Userid),
+		sm.From(dbmodels.Cars.Name(ctx)),
+		dbmodels.SelectWhere.Cars.Caruuid.EQ(carID),
+	)
+	result, err := bob.One(ctx, p.db, query, scan.SingleColumnMapper[int32])
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			err = ErrNotFound
+		}
+		return -1, err
+	}
+
+	return int64(result), err
+}
---
+++ backend/internal/pkg/repositories/car/postgres_integration_test.go
@@ -0,0 +1,67 @@
+package car
+
+import (
+	"context"
+	"testing"
+
+	"github.com/ParkWithEase/parkeasy/backend/internal/pkg/testutils"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/stdlib"
+	"github.com/stephenafamo/bob"
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+)
+
+func TestPostgresIntegration(t *testing.T) {
+	t.Skip("in progress")
+	t.Parallel()
+
+	testutils.Integration(t)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+
+	container, connString := testutils.CreatePostgresContainer(ctx, t)
+	t.Cleanup(func() { _ = container.Terminate(ctx) })
+	testutils.RunMigrations(t, connString)
+
+	err := container.Snapshot(ctx, postgres.WithSnapshotName(testutils.PostgresSnapshotName))
+	require.NoError(t, err, "could not snapshot db")
+
+	pool, err := pgxpool.New(ctx, connString)
+	require.NoError(t, err, "could not connect to db")
+	t.Cleanup(func() { pool.Close() })
+	db := bob.NewDB(stdlib.OpenDBFromPool(pool))
+
+	repo := NewPostgres(db)
+	_ = repo
+	// t.Run("basic add & get", func(t *testing.T) {
+	// 	t.Cleanup(func() {
+	// 		err := container.Restore(ctx, postgres.WithSnapshotName(testutils.PostgresSnapshotName))
+	// 		require.NoError(t, err, "could not restore db")
+	//
+	// 		// clear all idle connections
+	// 		// required since Restore() deletes the current DB
+	// 		pool.Reset()
+	// 	})
+	// 	const testEmail = "user@example.com"
+	// 	const testPasswordHash = "some hash"
+	//
+	// 	authUUID, err := repo.Create(ctx, testEmail, models.HashedPassword(testPasswordHash))
+	// 	require.NoError(t, err)
+	// 	assert.NotEqual(t, uuid.Nil, authUUID)
+	//
+	// 	identity, err := repo.Get(ctx, authUUID)
+	// 	require.NoError(t, err)
+	// 	assert.Equal(t, authUUID, identity.ID)
+	// 	assert.Equal(t, testEmail, identity.Email)
+	// 	assert.Equal(t, testPasswordHash, string(identity.PasswordHash))
+	// })
+	//
+	// t.Run("get non-existent", func(t *testing.T) {
+	// 	_, err := repo.Get(ctx, uuid.Nil)
+	// 	if assert.Error(t, err) {
+	// 		assert.ErrorIs(t, err, ErrIdentityNotFound)
+	// 	}
+	// })
+}
