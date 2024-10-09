d0e4ca7037c107d4d95467c025c7ea9f35fe07fb
TanoVip123 <nguyentano200@gmail.com>
2024-10-08 20:47:01 -0500

    feat: featch user profile

--- webapp/src/routes/profile/user-profile/+page.svelte
+++ webapp/src/routes/profile/user-profile/+page.svelte
@@ -1,6 +1,29 @@
 <script lang="ts">
     import { goto } from '$app/navigation';
-    import { BACKEND_SERVER } from '$lib/constants';
+    import { BACKEND_SERVER, INTERNAL_SERVER_ERROR } from '$lib/constants';
+    import type { UserProfile } from '$lib/types/user/user';
+    import { onMount } from 'svelte';
+
+    async function getUser(): Promise<UserProfile | null> {
+        try {
+            const response = await fetch(`${BACKEND_SERVER}/user`, {
+                credentials: 'include'
+            });
+
+            if (response.ok) {
+                console.log(response);
+                let user: UserProfile = (await response.json()) as UserProfile;
+                return user;
+            } else {
+                goto('/auth/login');
+                return null;
+            }
+        } catch (err: unknown) {
+            throw new Error(INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    let promise = getUser();
 
     async function logout() {
         try {
@@ -19,15 +42,33 @@
     }
 </script>
 
-<div class="container">
-    <h1>This is user profile</h1>
-</div>
+{#await promise}
+    <p>waiting...</p>
+{:then user}
+    {#if user != undefined}
+        <div class="container">
+            <div>
+                <h1>Name: {user.full_name}</h1>
+            </div>
+            <div>
+                <h1>Email: {user.email}</h1>
+            </div>
+        </div>
+    {:else}
+        <div>
+            {INTERNAL_SERVER_ERROR}
+        </div>
+    {/if}
+{/await}
 
-<style> 
-    .container{
+<style>
+    .container {
         display: flex;
-        flex-direction: row;
+        justify-items: flex-start;
+        flex-direction: column;
     }
 
-
-</style> 
+    h1 {
+        text-align: left;
+    }
+</style>
