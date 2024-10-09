f234be46c3374bbad0a362707f68c69ee60cf689
edwinduong <edwinduong23@gmail.com>
2024-10-08 19:15:46 -0500

    android: move state from LoginViewModel to LoginScreen

--- android/app/src/main/java/io/github/parkwithease/parkeasy/ui/login/LoginScreen.kt
+++ android/app/src/main/java/io/github/parkwithease/parkeasy/ui/login/LoginScreen.kt
@@ -23,7 +23,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -34,32 +37,32 @@ import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import io.github.parkwithease.parkeasy.R
-import io.github.parkwithease.parkeasy.ui.theme.ParkEasyTheme
+import io.github.parkwithease.parkeasy.model.LoginMode
 
-private data class LoginState(
+private data class LoginUiState(
     val name: String,
     val email: String,
     val password: String,
     val confirmPassword: String,
-    val matchingPasswords: Boolean,
     val loginMode: LoginMode,
-    val loggedIn: Boolean,
 )
 
-private data class LoginEvents(
+private data class LoginUiEvents(
     val onNameChange: (String) -> Unit,
     val onEmailChange: (String) -> Unit,
     val onPasswordChange: (String) -> Unit,
     val onConfirmPasswordChange: (String) -> Unit,
-    val onLoginPress: () -> Unit,
-    val onRegisterPress: () -> Unit,
-    val onRequestResetPress: () -> Unit,
-    val onSwitchModePress: (LoginMode) -> Unit,
+    val onSwitchMode: (LoginMode) -> Unit,
+)
+
+private data class LoginEvents(
+    val onLoginPress: (String, String) -> Unit,
+    val onRegisterPress: (String, String, String, String) -> Unit,
+    val onRequestResetPress: (String) -> Unit,
 )
 
 @Composable
@@ -70,34 +73,20 @@ fun LoginScreen(
 ) {
     val context = LocalContext.current
     val message by viewModel.message.collectAsState()
-    val state =
-        LoginState(
-            viewModel.name.collectAsState().value,
-            viewModel.email.collectAsState().value,
-            viewModel.password.collectAsState().value,
-            viewModel.confirmPassword.collectAsState().value,
-            viewModel.matchingPasswords.collectAsState().value,
-            viewModel.loginMode.collectAsState().value,
-            viewModel.loggedIn.collectAsState(false).value,
-        )
+    val loggedIn by viewModel.loggedIn.collectAsState(false)
     val events =
         LoginEvents(
-            viewModel::onNameChange,
-            viewModel::onEmailChange,
-            viewModel::onPasswordChange,
-            viewModel::onConfirmPasswordChange,
             viewModel::onLoginPress,
             viewModel::onRegisterPress,
             viewModel::onRequestResetPress,
-            viewModel::onSwitchModePress,
         )
     val latestOnLogin by rememberUpdatedState(onLogin)
-    LaunchedEffect(state.loggedIn) {
-        if (state.loggedIn) {
+    LaunchedEffect(loggedIn) {
+        if (loggedIn) {
             latestOnLogin()
         }
     }
-    LoginScreenInner(state, events, modifier)
+    LoginScreenInner(events, modifier)
     LaunchedEffect(message) {
         message.getContentIfNotHandled()?.let {
             Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
@@ -105,29 +94,8 @@ fun LoginScreen(
     }
 }
 
-@Preview
 @Composable
-private fun PreviewLoginScreenInner() {
-    val state =
-        LoginState(
-            "",
-            "",
-            "",
-            "",
-            matchingPasswords = false,
-            loginMode = LoginMode.REGISTER,
-            loggedIn = false,
-        )
-    val events = LoginEvents({}, {}, {}, {}, {}, {}, {}, {})
-    ParkEasyTheme { LoginScreenInner(state, events) }
-}
-
-@Composable
-private fun LoginScreenInner(
-    state: LoginState,
-    events: LoginEvents,
-    modifier: Modifier = Modifier,
-) {
+private fun LoginScreenInner(events: LoginEvents, modifier: Modifier = Modifier) {
     Surface(modifier) {
         Column {
             Row(
@@ -141,62 +109,66 @@ private fun LoginScreenInner(
                     modifier = Modifier.size(280.dp),
                 )
             }
-            Row(modifier = Modifier.weight(1f).fillMaxSize()) { LoginForm(state, events, 280.dp) }
+            Row(modifier = Modifier.weight(1f).fillMaxSize()) { LoginForm(events, 280.dp) }
         }
     }
 }
 
 @Composable
-private fun LoginForm(
-    state: LoginState,
-    events: LoginEvents,
-    width: Dp,
-    modifier: Modifier = Modifier,
-) {
+private fun LoginForm(events: LoginEvents, width: Dp, modifier: Modifier = Modifier) {
+    var name by rememberSaveable { mutableStateOf("") }
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    var confirmPassword by rememberSaveable { mutableStateOf("") }
+    var loginMode by rememberSaveable { mutableStateOf(LoginMode.LOGIN) }
+    val uiState = LoginUiState(name, email, password, confirmPassword, loginMode)
+    val uiEvents =
+        LoginUiEvents(
+            { name = it },
+            { email = it },
+            { password = it },
+            { confirmPassword = it },
+            { loginMode = it },
+        )
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(4.dp),
         modifier = modifier.fillMaxSize(),
     ) {
-        LoginFields(state, events, Modifier.width(width))
-        AnimatedVisibility(state.loginMode != LoginMode.REGISTER) {
-            SwitchRequestResetText(
+        LoginFields(uiState, uiEvents, width)
+        Button(
+            onClick = {
+                when (loginMode) {
+                    LoginMode.LOGIN -> events.onLoginPress(email, password)
+                    LoginMode.REGISTER ->
+                        events.onRegisterPress(name, email, password, confirmPassword)
+                    LoginMode.FORGOT -> events.onRequestResetPress(email)
+                }
+            },
+            modifier = Modifier.width(width),
+        ) {
+            Text(
                 stringResource(
-                    if (state.loginMode == LoginMode.FORGOT) R.string.return_login
-                    else R.string.forgot_password
-                ),
-                if (state.loginMode == LoginMode.FORGOT) LoginMode.LOGIN else LoginMode.FORGOT,
-                events.onSwitchModePress,
-                Modifier.width(width),
+                    when (loginMode) {
+                        LoginMode.LOGIN -> R.string.login
+                        LoginMode.REGISTER -> R.string.register
+                        LoginMode.FORGOT -> R.string.forgot_password
+                    }
+                )
             )
         }
-        LoginButton(
-            stringResource(
-                when (state.loginMode) {
-                    LoginMode.LOGIN -> R.string.login
-                    LoginMode.REGISTER -> R.string.register
-                    LoginMode.FORGOT -> R.string.forgot_password
-                }
-            ),
-            when (state.loginMode) {
-                LoginMode.LOGIN -> events.onLoginPress
-                LoginMode.REGISTER -> events.onRegisterPress
-                LoginMode.FORGOT -> events.onRequestResetPress
-            },
-            Modifier.width(width),
-        )
-        AnimatedVisibility(state.loginMode != LoginMode.FORGOT) {
+        AnimatedVisibility(loginMode != LoginMode.FORGOT) {
             SwitchRegisterText(
                 stringResource(
-                    if (state.loginMode == LoginMode.REGISTER) R.string.already_registered
+                    if (loginMode == LoginMode.REGISTER) R.string.already_registered
                     else R.string.not_registered
                 ),
                 stringResource(
-                    if (state.loginMode == LoginMode.REGISTER) R.string.login_instead
+                    if (loginMode == LoginMode.REGISTER) R.string.login_instead
                     else R.string.register_instead
                 ),
-                if (state.loginMode == LoginMode.REGISTER) LoginMode.LOGIN else LoginMode.REGISTER,
-                events.onSwitchModePress,
+                if (loginMode == LoginMode.REGISTER) LoginMode.LOGIN else LoginMode.REGISTER,
+                { loginMode = it },
                 Modifier.width(width),
             )
         }
@@ -204,7 +176,12 @@ private fun LoginForm(
 }
 
 @Composable
-private fun LoginFields(state: LoginState, events: LoginEvents, modifier: Modifier = Modifier) {
+private fun LoginFields(
+    state: LoginUiState,
+    events: LoginUiEvents,
+    width: Dp,
+    modifier: Modifier = Modifier,
+) {
     Column(modifier) {
         AnimatedVisibility(state.loginMode == LoginMode.REGISTER) {
             LoginField(
@@ -239,8 +216,19 @@ private fun LoginFields(state: LoginState, events: LoginEvents, modifier: Modifi
                 stringResource(R.string.confirm_password),
                 ImageVector.vectorResource(R.drawable.password),
                 KeyboardOptions(keyboardType = KeyboardType.Password),
-                isError = !state.matchingPasswords,
+                isError = state.password != state.confirmPassword,
                 visualTransformation = PasswordVisualTransformation(),
+            )
+        }
+        AnimatedVisibility(state.loginMode != LoginMode.REGISTER) {
+            SwitchRequestResetText(
+                stringResource(
+                    if (state.loginMode == LoginMode.FORGOT) R.string.return_login
+                    else R.string.forgot_password
+                ),
+                if (state.loginMode == LoginMode.FORGOT) LoginMode.LOGIN else LoginMode.FORGOT,
+                events.onSwitchMode,
+                Modifier.width(width),
             )
         }
     }
@@ -268,11 +256,6 @@ private fun LoginField(
         singleLine = true,
         modifier = modifier,
     )
-}
-
-@Composable
-private fun LoginButton(label: String, onClick: () -> Unit, modifier: Modifier = Modifier) {
-    Button(onClick = { onClick() }, modifier = modifier) { Text(label) }
 }
 
 @Composable
--- android/app/src/main/java/io/github/parkwithease/parkeasy/ui/login/LoginViewModel.kt
+++ android/app/src/main/java/io/github/parkwithease/parkeasy/ui/login/LoginViewModel.kt
@@ -13,59 +13,18 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.runBlocking
 
-enum class LoginMode {
-    LOGIN,
-    REGISTER,
-    FORGOT,
-}
-
 @HiltViewModel
 class LoginViewModel
 @Inject
 constructor(authRepo: AuthRepository, private val userRepo: UserRepository) : ViewModel() {
-    private val _name = MutableStateFlow("")
-    val name = _name.asStateFlow()
-
-    private val _email = MutableStateFlow("")
-    val email = _email.asStateFlow()
-
-    private val _password = MutableStateFlow("")
-    val password = _password.asStateFlow()
-
-    private val _confirmPassword = MutableStateFlow("")
-    val confirmPassword = _confirmPassword.asStateFlow()
-
-    private val _matchingPasswords = MutableStateFlow(true)
-    val matchingPasswords = _matchingPasswords.asStateFlow()
-
-    private val _loginMode = MutableStateFlow(LoginMode.LOGIN)
-    val loginMode = _loginMode.asStateFlow()
-
     val loggedIn = authRepo.statusFlow
 
     private val _message = MutableStateFlow(Event.initial(""))
     val message = _message.asStateFlow()
 
-    fun onNameChange(input: String) {
-        _name.value = input
-    }
-
-    fun onEmailChange(input: String) {
-        _email.value = input
-    }
-
-    fun onPasswordChange(input: String) {
-        _password.value = input
-    }
-
-    fun onConfirmPasswordChange(input: String) {
-        _confirmPassword.value = input
-        _matchingPasswords.value = _password.value == _confirmPassword.value
-    }
-
-    fun onLoginPress() {
+    fun onLoginPress(email: String, password: String) {
         runBlocking {
-            if (userRepo.login(LoginCredentials(_email.value, _password.value))) {
+            if (userRepo.login(LoginCredentials(email, password))) {
                 _message.value = Event("Logged in successfully")
             } else {
                 _message.value = Event("Error logging in")
@@ -73,14 +32,10 @@ constructor(authRepo: AuthRepository, private val userRepo: UserRepository) : Vi
         }
     }
 
-    fun onRegisterPress() {
-        if (_password.value == _confirmPassword.value) {
+    fun onRegisterPress(name: String, email: String, password: String, confirmPassword: String) {
+        if (password == confirmPassword) {
             runBlocking {
-                if (
-                    userRepo.register(
-                        RegistrationCredentials(_name.value, _email.value, _password.value)
-                    )
-                ) {
+                if (userRepo.register(RegistrationCredentials(name, email, password))) {
                     _message.value = Event("Registered successfully")
                 } else {
                     _message.value = Event("Error registering")
@@ -91,17 +46,13 @@ constructor(authRepo: AuthRepository, private val userRepo: UserRepository) : Vi
         }
     }
 
-    fun onRequestResetPress() {
+    fun onRequestResetPress(email: String) {
         runBlocking {
-            if (userRepo.requestReset(ResetCredentials(_email.value))) {
+            if (userRepo.requestReset(ResetCredentials(email))) {
                 _message.value = Event("Reset email sent\nJk... we're working on it")
             } else {
                 _message.value = Event("Error resetting password")
             }
         }
-    }
-
-    fun onSwitchModePress(loginMode: LoginMode) {
-        _loginMode.value = loginMode
     }
 }
