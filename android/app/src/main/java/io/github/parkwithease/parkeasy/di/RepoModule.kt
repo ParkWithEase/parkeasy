0274490b84565e6840f6f692467273f99fd531da
edwinduong <edwinduong23@gmail.com>
2024-10-08 20:13:40 -0500

    android: combine AuthModule and UserModule into RepoModule

--- android/app/src/main/java/io/github/parkwithease/parkeasy/di/RepoModule.kt
+++ android/app/src/main/java/io/github/parkwithease/parkeasy/di/RepoModule.kt
@@ -4,12 +4,15 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import io.github.parkwithease.parkeasy.data.local.AuthRepository
+import io.github.parkwithease.parkeasy.data.local.AuthRepositoryImpl
 import io.github.parkwithease.parkeasy.data.remote.UserRepository
 import io.github.parkwithease.parkeasy.data.remote.UserRepositoryImpl
 
 @Module
 @InstallIn(SingletonComponent::class)
 @Suppress("Unused")
-interface UserModule {
+interface RepoModule {
+    @Binds fun bindAuthRepository(authRepositoryImpl: AuthRepositoryImpl): AuthRepository
     @Binds fun bindUserRepository(userRepositoryImpl: UserRepositoryImpl): UserRepository
 }
