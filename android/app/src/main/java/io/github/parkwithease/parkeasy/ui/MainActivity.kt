7c424969ebb61e25818dcb39b9768547c444c656
edwinduong <edwinduong23@gmail.com>
2024-10-19 02:06:28 -0500

    android: add SnackbarHostState and data classes

--- android/app/src/main/java/io/github/parkwithease/parkeasy/ui/MainActivity.kt
+++ android/app/src/main/java/io/github/parkwithease/parkeasy/ui/MainActivity.kt
@@ -6,21 +6,44 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.parkwithease.parkeasy.ui.navbar.NavBar
 import io.github.parkwithease.parkeasy.ui.theme.ParkEasyTheme
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { ParkEasyTheme { MainNavGraph() } }
         setContent {
             ParkEasyTheme {
+                val snackbarHostState = remember { SnackbarHostState() }
+                val scope = rememberCoroutineScope()
+                ObserveAsEvents(flow = SnackbarController.events, snackbarHostState) { event ->
+                    scope.launch {
+                        snackbarHostState.currentSnackbarData?.dismiss()
+                        val result =
+                            snackbarHostState.showSnackbar(
+                                message = event.message,
+                                actionLabel = event.action?.name,
+                                duration = SnackbarDuration.Short,
+                            )
+                        if (result == SnackbarResult.ActionPerformed) {
+                            event.action?.action?.invoke()
+                        }
+                    }
+                }
                 Scaffold(
                     bottomBar = { NavBar() },
+                    snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
                 ) { innerPadding ->
                     MainNavGraph(modifier = Modifier.padding(innerPadding))
                 }
