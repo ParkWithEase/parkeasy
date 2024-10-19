3ca17a00504586e85f2f688fc48b83576c06ad8c
mayallhs <mayallh10@gmail.com>
2024-10-19 01:57:29 -0500

    Edited the navbar

--- webapp/src/lib/components/navbar.svelte
+++ webapp/src/lib/components/navbar.svelte
@@ -4,60 +4,41 @@
     Header,
     HeaderUtilities,
     HeaderGlobalAction,
-    SideNav,
-    SideNavItems,
-    SideNavMenu,
-    SideNavMenuItem,
-    SideNavLink,
-    SkipToContent,
-    Content,
-    Grid,
-    Row,
-    Column,
+    SkipToContent
     } from "carbon-components-svelte";
+    import Logout from "../../../node_modules/carbon-icons-svelte/lib/Logout.svelte";
+    import User from "../../../node_modules/carbon-icons-svelte/lib/User.svelte";
+    import Map from "../../../node_modules/carbon-icons-svelte/lib/Map.svelte";
+    import MobileAdd from "carbon-icons-svelte/lib/MobileAdd.svelte";
 </script>
 
 <svelte:window />
-<nav class="navbar">
+
+<Header>
     <a href="/#"><img src={logo} alt="parkeasy-logo" class="text-logo" /></a>
-    <a class="nav-link" href="/app/profile/user-profile">book a spot</a>
-    <a class="nav-link" href="/app/profile/user-profile">list your spot</a>
-    <a class="nav-link" href="/app/profile/user-profile">my profile</a>
-    <a class="nav-link" href="/app/">about us</a>
-</nav>
+    <svelte:fragment slot="skip-to-content">
+      <SkipToContent />
+    </svelte:fragment>
+    <HeaderUtilities>
+      <HeaderGlobalAction
+        iconDescription="Explore open spots"
+        tooltipAlignment="start"
+        icon={Map}
+        href="/#"
+      />
+      <HeaderGlobalAction iconDescription="Add your spot" icon={MobileAdd} href="/app/profile/user-profile"/>
+      <HeaderGlobalAction iconDescription="Profile" icon={User} href="/app/profile/user-profile"/>
+      <HeaderGlobalAction
+        iconDescription="Log out"
+        tooltipAlignment="end"
+        icon={Logout}
+      />
+    </HeaderUtilities>
+</Header>
 
 <style>
-    .navbar {
-        position: fixed;
-        top: 0;
-        width: 100%;
-        height: 7rem;
-        background-color: #000;
-        color: #fff;
-        z-index: 999;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-evenly;
-        align-items: center;
-    }
-
-    .nav-link {
-        background-color: white;
-        padding: 1rem 2rem;
-        border-radius: 20px;
-        font-size: 1.3rem;
-        font-weight: bold;
-        color: black;
-        text-decoration: none;
-    }
-
-    .nav-link:hover {
-        color: #7ed957;
-        background-color: rgb(54, 54, 54);
-        transition: 0.3s;
-    }
 
     .text-logo {
-        max-height: 7rem;
+        max-height: 4rem;
     }
 </style>
