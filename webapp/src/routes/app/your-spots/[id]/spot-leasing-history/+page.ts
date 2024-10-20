523cd572f4c8b7e0dcb8c39d7f6871857daebb28
TanoVip123 <nguyentano200@gmail.com>
2024-10-20 12:52:38 -0500

    feat: frontend display leasing history per parking spot

---
+++ webapp/src/routes/app/your-spots/[id]/spot-leasing-history/+page.ts
@@ -0,0 +1,73 @@
+import type { PageLoad } from "./$types";
+const test_data = [{
+    id: '1',
+    spot: {
+        street_address: '230 Portage',
+        postal_code: 'RRRRRR',
+        city: "Winnipeg",
+        country_code: "CA",
+    },
+    car: {
+        license_plate: "ABCDE",
+        color: "Red",
+        model: "Honda",
+        make: "random make",
+    },
+    time: {
+        date: new Date("2024-10-1"),
+        start: "12:00",
+        end: "14:00",
+    },
+    paid_amount: 100,
+},
+{
+    id: '2',
+    spot: {
+        street_address: '230 Portage',
+        postal_code: 'RRRRRR',
+        city: "Winnipeg",
+        country_code: "CA",
+    },
+    car: {
+        license_plate: "VCDES",
+        color: "Red",
+        model: "Honda",
+        make: "random make",
+    },
+    time: {
+        date: new Date("2024-10-4"),
+        start: "2:00",
+        end: "14:00",
+    },
+    paid_amount: 150,
+},
+{
+    id: '3',
+    spot: {
+        street_address: '230 Portage',
+        postal_code: 'RRRRRR',
+        city: "Winnipeg",
+        country_code: "CA",
+    },
+    car: {
+        license_plate: "RacerX",
+        color: "Blue",
+        model: "Honda",
+        make: "random make",
+    },
+    time: {
+        date: new Date("2024-10-10"),
+        start: "12:00",
+        end: "14:00",
+    },
+    paid_amount: 240,
+},
+]
+
+export const load: PageLoad = async ({ fetch }) => {
+    return {
+        transactions: test_data,
+        hasNext: undefined,
+        paging: undefined,
+    }
+}
