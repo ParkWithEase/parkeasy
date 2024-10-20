10db0e55bd6c4295d32c37c9437b74c2ba7dcd60
edwinduong <69070602+edwinduong@users.noreply.github.com>
2024-10-20 14:19:00 -0500

    Android: Replace Toasts with Snackbars (#181)
    * android: update runBlocking calls to CoroutineScope launch calls
    
    * android: move Scaffold to root of MainActivity
    
    * android: add SnackbarHostState and data classes
    
    * android: add ListScreen, MapScreen
    
    * android: add inner navigation between List, Map, and Profile
    
    * android: replace Toasts with Snackbars
    
    * android: replace CoroutineScope with viewModelScope
    
    * android: inject SnackbarHostState instead of SnackbarHost
    
    * android: updated requestReset success from HTTP OK to HTTP Accepted
    
    * android: remove global SnackbarController, inject showSnackbar instead

---
+++ android/app/src/main/java/io/github/parkwithease/parkeasy/ui/map/MapScreen.kt
@@ -0,0 +1,15 @@
+package io.github.parkwithease.parkeasy.ui.map
+
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun MapScreen(
+    modifier: Modifier = Modifier,
+    @Suppress("unused") viewModel: MapViewModel = hiltViewModel<MapViewModel>(),
+) {
+    Surface(modifier) { Text("Map") }
+}
--- android/app/src/main/java/io/github/parkwithease/parkeasy/ui/navbar/NavBar.kt
+++ android/app/src/main/java/io/github/parkwithease/parkeasy/ui/navbar/NavBar.kt
@@ -1,5 +1,8 @@
 package io.github.parkwithease.parkeasy.ui.navbar
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
@@ -19,6 +22,9 @@ import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun NavBar(
+    navigateToList: () -> Unit,
+    navigateToMap: () -> Unit,
+    navigateToProfile: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: NavBarViewModel = hiltViewModel<NavBarViewModel>(),
 ) {
@@ -26,20 +32,32 @@ fun NavBar(
     val items = listOf("List", "Map", "Profile")
     val selectedIcons = listOf(Icons.Filled.Menu, Icons.Filled.Place, Icons.Filled.Person)
     val unselectedIcons = listOf(Icons.Outlined.Menu, Icons.Outlined.Place, Icons.Outlined.Person)
+    val loggedIn by viewModel.loggedIn.collectAsState(false)
+    val navigateTo = listOf(navigateToList, navigateToMap, navigateToProfile)
 
-    NavigationBar(modifier) {
-        items.forEachIndexed { index, item ->
-            NavigationBarItem(
-                icon = {
-                    Icon(
-                        if (selectedItem == index) selectedIcons[index] else unselectedIcons[index],
-                        contentDescription = item,
-                    )
-                },
-                label = { Text(item) },
-                selected = selectedItem == index,
-                onClick = { viewModel.onClick(index) },
-            )
+    AnimatedVisibility(
+        visible = loggedIn,
+        enter = slideInVertically(initialOffsetY = { it / 2 }),
+        exit = slideOutVertically(targetOffsetY = { it / 2 }),
+    ) {
+        NavigationBar(modifier) {
+            items.forEachIndexed { index, item ->
+                NavigationBarItem(
+                    icon = {
+                        Icon(
+                            if (selectedItem == index) selectedIcons[index]
+                            else unselectedIcons[index],
+                            contentDescription = item,
+                        )
+                    },
+                    label = { Text(item) },
+                    selected = selectedItem == index,
+                    onClick = {
+                        viewModel.onClick(index)
+                        navigateTo[index]()
+                    },
+                )
+            }
         }
     }
 }
