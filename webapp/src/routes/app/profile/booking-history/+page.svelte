c7577049fdb1e4f732324600c1a08f778cd39bf3
Harkeerat S. <mayallh10@gmail.com>
2024-10-20 22:01:17 -0500

    mayallhs/restyle user page (#191)
    * Refactored code
    
    * Edited the navbar
    
    * Restyled user profile page, added mock data displays to history tabs
    
    * Added layout.svelte to make navbar appear on each app route
    
    * Fixed lint errors
    
    * Added navbar to all app routes
    
    * feat: NavBar to show active url
    
    * Made the history lists into components
    
    * Added component file to git
    
    * Fixed lint
    
    * Removed extra padding and usused css
    
    * removed unusued css
    
    * fixed spacing on your-cars page
    
    * Added image to the lists for booking and leasing history in profile
    
    * Removed type Booking from mocks
    
    * Fixed lint errors
    
    * Moved image into child componet and added approprapriate spacing
    
    * Fixed lint
    
    * Removed svelte ignore comment
    
    ---------
    
    Co-authored-by: TanoVip123 <nguyentano200@gmail.com>
    Co-authored-by: TanoVip123 <42956543+TanoVip123@users.noreply.github.com>

--- webapp/src/routes/app/profile/booking-history/+page.svelte
+++ webapp/src/routes/app/profile/booking-history/+page.svelte
@@ -1,1 +1,46 @@
-<h1>This is booking history</h1>
+<script lang="ts">
+    import BookingHistoryDisplay from '$lib/components/history-list-component/history-list-display.svelte';
+    let bookingList = [
+        {
+            booking_id: '000101010',
+            address: "Chancellor's Circle",
+            postalCode: 'X1X1X1',
+            price: 10.26,
+            time_from: 800,
+            time_to: 1000,
+            duration: 2
+        },
+        {
+            booking_id: '000101099',
+            address: 'St. Vital',
+            postalCode: 'ABC123',
+            price: 31.48,
+            time_from: 1600,
+            time_to: 2030,
+            duration: 4.5
+        }
+    ];
+</script>
+
+<div class="list-container">
+    {#key bookingList}
+        {#each bookingList as booking}
+            <div class="booking-info-container">
+                <BookingHistoryDisplay {booking}></BookingHistoryDisplay>
+            </div>
+        {/each}
+    {/key}
+</div>
+
+<style>
+    :global(.bx--content) {
+        padding: 0rem;
+    }
+
+    .booking-info-container {
+        display: flex;
+        flex-direction: row;
+        color: rgb(0, 0, 0);
+        border: 1px solid #cfcfcfcf;
+    }
+</style>
