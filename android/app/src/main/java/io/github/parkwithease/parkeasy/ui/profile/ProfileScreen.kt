f33514d602f997f6f83a0bc6ee954a7f3e99d231
edwinduong <edwinduong23@gmail.com>
2024-10-22 20:38:18 -0500

    android: update styling on ProfileScreen

--- android/app/src/main/java/io/github/parkwithease/parkeasy/ui/profile/ProfileScreen.kt
+++ android/app/src/main/java/io/github/parkwithease/parkeasy/ui/profile/ProfileScreen.kt
@@ -3,7 +3,10 @@ package io.github.parkwithease.parkeasy.ui.profile
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,6 +18,7 @@ import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import io.github.parkwithease.parkeasy.R
 import io.github.parkwithease.parkeasy.model.Profile
@@ -41,27 +45,49 @@ fun ProfileScreen(profile: Profile, onLogoutClick: () -> Unit, modifier: Modifie
     Surface(modifier) {
         Column(
             modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
+            verticalArrangement = Arrangement.spacedBy(4.dp, Alignment.CenterVertically),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             ProfileDetails(profile)
-            Button(onClick = onLogoutClick) { Text("Logout 2") }
+            LogoutButton(onLogoutClick)
         }
     }
 }
 
 @Composable
 fun ProfileDetails(profile: Profile, modifier: Modifier = Modifier) {
-    Column(verticalArrangement = Arrangement.SpaceBetween, modifier = modifier) {
+    Column(modifier = modifier, verticalArrangement = Arrangement.spacedBy(4.dp)) {
         OutlinedTextField(
             value = profile.name,
             onValueChange = {},
+            modifier = Modifier.width(280.dp),
+            readOnly = true,
             label = { Text(stringResource(R.string.name)) },
+            singleLine = true,
         )
         OutlinedTextField(
             value = profile.email,
             onValueChange = {},
+            modifier = Modifier.width(280.dp),
+            readOnly = true,
             label = { Text(stringResource(R.string.email)) },
+            singleLine = true,
         )
     }
+}
+
+@Composable
+fun LogoutButton(onLogoutClick: () -> Unit, modifier: Modifier = Modifier) {
+    Button(
+        onClick = onLogoutClick,
+        modifier = modifier.width(280.dp),
+        colors =
+            ButtonColors(
+                containerColor = MaterialTheme.colorScheme.error,
+                contentColor = MaterialTheme.colorScheme.onError,
+                disabledContainerColor = MaterialTheme.colorScheme.errorContainer,
+                disabledContentColor = MaterialTheme.colorScheme.onErrorContainer,
+            ),
+        content = { Text(stringResource(R.string.logout)) },
+    )
 }
