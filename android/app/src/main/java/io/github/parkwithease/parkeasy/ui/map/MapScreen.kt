961973a16023ffdafb0b40cad5687e996713cd20
edwinduong <edwinduong23@gmail.com>
2024-10-23 12:01:52 -0500

    android: add MapScreen map

--- android/app/src/main/java/io/github/parkwithease/parkeasy/ui/map/MapScreen.kt
+++ android/app/src/main/java/io/github/parkwithease/parkeasy/ui/map/MapScreen.kt
@@ -1,15 +1,24 @@
 package io.github.parkwithease.parkeasy.ui.map
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.maplibre.compose.MapView
+import com.maplibre.compose.rememberSaveableMapViewCamera
 
 @Composable
 fun MapScreen(
     modifier: Modifier = Modifier,
     @Suppress("unused") viewModel: MapViewModel = hiltViewModel<MapViewModel>(),
 ) {
-    Surface(modifier) { Text("Map") }
+    val mapViewCamera = rememberSaveableMapViewCamera()
+    Surface(modifier = modifier) {
+        MapView(
+            modifier = Modifier.fillMaxSize(),
+            styleUrl = "https://demotiles.maplibre.org/style.json",
+            camera = mapViewCamera,
+        )
+    }
 }
