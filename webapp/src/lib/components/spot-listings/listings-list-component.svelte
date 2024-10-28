de757d0114d47e5a8c6e65500ed3e7297178b0c9
mayallhs <mayallh10@gmail.com>
2024-10-27 23:00:06 -0500

    Merge commit

---
+++ webapp/src/lib/components/spot-listings/listings-list-component.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    export let listing;
+</script>
+
+<div class="listing-info">
+    <div>
+        <p class="listing-head">{listing.address}</p>
+        <p>{listing.postalCode}</p>
+    </div>
+    <div>
+        <p class="listing-data">From: {listing.time_from}</p>
+        <p class="listing-data">To: {listing.time_to}</p>
+    </div>
+</div>
+
+<style>
+    .listing-info {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+    }
+    .listing-head {
+        font-size: 1.5rem;
+        border-bottom: 2px solid white;
+    }
+    .listing-data {
+        font-size: 1rem;
+    }
+</style>
--- webapp/src/routes/app/+page.svelte
+++ webapp/src/routes/app/+page.svelte
@@ -1,6 +1,28 @@
 <script lang="ts">
     import Navbar from '$lib/components/navbar.svelte';
-    import { MapLibre } from 'svelte-maplibre';
+    import { MapLibre, DefaultMarker, Popup, GeolocateControl } from 'svelte-maplibre';
+    import ListingsListComponent from '$lib/components/spot-listings/listings-list-component.svelte';
+    import { MapLibreSearchControl } from "@stadiamaps/maplibre-search-box";
+
+    const coordsList: number[][] = [
+        [-97.133290, 49.808856],
+        [-97.199808, 49.811791]
+    ];
+
+    const lngLatList: [number, number][] = coordsList as [number, number][];
+    let listings= [
+        {
+        lngLat: lngLatList[0],
+        label: 'Engineering Building',
+        name: 'Engineering Building UofM',
+        },
+        {
+        lngLat: lngLatList[1],
+        label: 'Whyte Ridge',
+        name: 'Whyte Ridge',
+        }
+    ];
+
 </script>
 
 <Navbar />
@@ -8,16 +30,37 @@
 <body>
     <div class="container">
         <div class="listings">
-            <h2>Listings go here!</h2>
+            {#key listings}
+                {#each listings as listing}
+                    <div class="booking-info-container">
+                        <ListingsListComponent {listing}></ListingsListComponent>
+                    </div>
+                {/each}
+            {/key}
         </div>
         <div class="map-view">
             <MapLibre
                 center={[-97.1, 49.9]}
                 zoom={10}
                 class="map"
-                standardControls
                 style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
-            />
+            >
+                <GeolocateControl position="top-left" fitBoundsOptions={{ maxZoom: 12 }} />
+                {#each listings as { lngLat, label, name }}
+                    <DefaultMarker {lngLat} draggable>
+                        <Popup
+                            offset={[0, -10]}
+                            on:open={async () => {
+                                const resp = await fetch(`/examples/popup_remote/${name}`);
+                                const result = await resp.json();
+                            }}
+                        >
+                            <div class="text-lg font-bold">{name}</div>
+                            <h1>Listing data here</h1>
+                        </Popup>
+                    </DefaultMarker>
+                {/each}
+            </MapLibre>
         </div>
     </div>
 </body>
@@ -26,7 +69,6 @@
     .container {
         display: flex;
         flex-direction: row;
-        margin: 8.5rem 3rem;
         max-height: fit-content;
     }
 
@@ -35,19 +77,16 @@
         width: 30%;
         min-height: 100%;
         flex-direction: column;
-        font-size: 1.2rem;
-        font-weight: bold;
-        align-items: center;
-        justify-content: center;
+        padding: 0.5rem;
         background-color: rgb(186, 214, 183);
     }
 
     .map-view {
-        width: 75%;
+        width: 100%;
         background-color: white;
     }
 
     :global(.map) {
-        height: 75vh;
+        height: 80vh;
     }
 </style>
