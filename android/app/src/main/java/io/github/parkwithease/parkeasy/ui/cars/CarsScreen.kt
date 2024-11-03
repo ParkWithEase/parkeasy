7ab96284389c50d48294b54342597880cb90c8f4
edwinduong <69070602+edwinduong@users.noreply.github.com>
2024-11-03 01:36:44 -0600

    android: update CarsScreen to use common PullToRefreshBox

--- android/app/src/main/java/io/github/parkwithease/parkeasy/ui/cars/CarsScreen.kt
+++ android/app/src/main/java/io/github/parkwithease/parkeasy/ui/cars/CarsScreen.kt
@@ -1,82 +1,65 @@
 package io.github.parkwithease.parkeasy.ui.cars
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ListItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.pulltorefresh.PullToRefreshBox
-import androidx.compose.material3.pulltorefresh.PullToRefreshState
-import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import io.github.parkwithease.parkeasy.common.PullToRefreshBox
 import io.github.parkwithease.parkeasy.model.Car
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CarsScreen(
-    onSelectCar: (Car) -> Unit,
+    onCarClick: (Car) -> Unit,
     modifier: Modifier = Modifier,
     viewModel: CarsViewModel = hiltViewModel(),
 ) {
     val cars by viewModel.cars.collectAsState()
     val isRefreshing by viewModel.isRefreshing.collectAsState()
-    val pullToRefreshState = rememberPullToRefreshState()
-    CarsScreen(cars, isRefreshing, viewModel::onRefresh, onSelectCar, modifier, pullToRefreshState)
+
     LaunchedEffect(Unit) { viewModel.onRefresh() }
+
+    CarsScreen(cars, onCarClick, isRefreshing, viewModel::onRefresh, modifier)
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CarsScreen(
     cars: List<Car>,
+    onCarClick: (Car) -> Unit,
     isRefreshing: Boolean,
     onRefresh: () -> Unit,
-    onSelectCar: (Car) -> Unit,
     modifier: Modifier = Modifier,
-    pullToRefreshState: PullToRefreshState = rememberPullToRefreshState(),
 ) {
-    Surface(modifier) {
-        Box {
+    Scaffold(modifier) { innerPadding ->
+        Surface(Modifier.padding(innerPadding)) {
             PullToRefreshBox(
+                items = cars,
+                onClick = onCarClick,
                 isRefreshing = isRefreshing,
                 onRefresh = onRefresh,
-                state = pullToRefreshState,
                 modifier = Modifier.padding(4.dp),
-            ) {
-                LazyColumn(
-                    Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.spacedBy(8.dp),
-                ) {
-                    items(cars) { car ->
-                        ListItem({
-                            Card(
-                                onClick = { onSelectCar(car) },
-                                modifier = Modifier.fillMaxWidth().padding(8.dp, 0.dp),
-                            ) {
-                                Text(car.details.color)
-                                Text(car.details.model)
-                                Text(car.details.make)
-                                Text(car.details.licensePlate)
-                            }
-                        })
-                    }
-                }
-            }
+                card = { car, onClick -> CarCard(car, onClick) },
+            )
         }
+    }
+}
+
+@Composable
+fun CarCard(car: Car, onClick: (Car) -> Unit, modifier: Modifier = Modifier) {
+    Card(onClick = { onClick(car) }, modifier = modifier.fillMaxWidth().padding(8.dp, 0.dp)) {
+        Text(car.details.color)
+        Text(car.details.model)
+        Text(car.details.make)
+        Text(car.details.licensePlate)
     }
 }
