a0a98ed1b0c4bf2edd04f3bc25c322609e6ce683
edwinduong <69070602+edwinduong@users.noreply.github.com>
2024-11-03 14:47:53 -0600

    android: update CarCard styling

--- android/app/src/main/java/io/github/parkwithease/parkeasy/ui/cars/CarsScreen.kt
+++ android/app/src/main/java/io/github/parkwithease/parkeasy/ui/cars/CarsScreen.kt
@@ -1,8 +1,11 @@
 package io.github.parkwithease.parkeasy.ui.cars
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
@@ -30,12 +33,14 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import io.github.parkwithease.parkeasy.R
 import io.github.parkwithease.parkeasy.common.PullToRefreshBox
 import io.github.parkwithease.parkeasy.model.Car
+import io.github.parkwithease.parkeasy.ui.theme.Typography
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -117,11 +122,20 @@ fun CarsScreen(
 
 @Composable
 fun CarCard(car: Car, onClick: (Car) -> Unit, modifier: Modifier = Modifier) {
-    Card(onClick = { onClick(car) }, modifier = modifier.fillMaxWidth().padding(8.dp, 0.dp)) {
-        Text(car.details.color)
-        Text(car.details.model)
-        Text(car.details.make)
-        Text(car.details.licensePlate)
+    Card(onClick = { onClick(car) }, modifier = modifier.fillMaxWidth().padding(4.dp, 0.dp)) {
+        Row(modifier = Modifier.padding(8.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Image(
+                    painter = painterResource(R.drawable.wordmark_outlined),
+                    contentDescription = null,
+                    modifier = Modifier.heightIn(max = 64.dp),
+                )
+            }
+            Column(horizontalAlignment = Alignment.End, modifier = Modifier.weight(1f)) {
+                Text(text = car.details.licensePlate, style = Typography.headlineLarge)
+                Text(car.details.color + ' ' + car.details.make + " " + car.details.model)
+            }
+        }
     }
 }
 
