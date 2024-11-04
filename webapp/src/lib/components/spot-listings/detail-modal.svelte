8aa46b4b49a33e4049920ec22478899928eab7f3
Harkeerat S. <mayallh10@gmail.com>
2024-11-04 00:08:23 -0600

    Added functionality to Listing view and Map explore view (#226)
    * feat: implement seller spot display + adding form
    
    * feat: Layout for seller parking spot
    
    * feat: frontend display leasing history per parking spot
    
    * feat: add availability table format
    
    * draft: on-going availability table
    
    * feat: display timeslot data on the table
    
    * refactor: refactor the function to move date to next week
    
    * refactor: handle stuff in UTC instead of local time
    
    * feat: availability table interaction finished
    
    * fix: fix a bug where side nav bar disappear when browser shrink
    
    * refactor: refactor time table function into an util files for reusability
    
    * fix: Fix a bug where the removed slot is not updated correctly
    
    * feat: Parking spot spot multi-form
    
    * feat: coalesce key before call geoencoding autocompletion
    
    * feat: Add price field and enhance form reset mechanism
    
    * lint: addressing linting error
    
    * Merge commit
    
    * Removed unused dependancies
    
    * fix: Delaying converting to UTC
    
    We will convert to UTC time upon before making request only. Doing editing in UTC time is not
    intuitive to user so we will use local time zone for editing and convert that to UTC before sending it to the backend.
    For the record, when receiving time slot from backend, we will also convert it to local first before displaying
    
    * Added the listings and map, also added detail modal
    
    * Added the detail modal
    
    * Refactored files
    
    * Added fetch for search results for geocoding
    
    * feat: Hook up Parking Spot API
    POST /spots
    GET /user/spots
    GET /spots/{id}
    GET /spots/{id}/availability
    
    Also change the mechanism for availability table from storing an array of edit records to storing multiple table of records instead.
    
    * fix: update frontend with backend GET id bug fix
    
    * lint: fix linting
    
    * Added functionality to listing view and map view
    
    * Modified listing view and map view
    
    * Added env file and refactored
    
    * Removed possible exposure
    
    * Addressed comments on PR
    
    * Fixed linting issues
    
    * Added type for listing in detail modal
    
    * Added constant for initial zoom in the modal
    
    * Moved listing detail modal into the listings container
    
    * installed dependancies
    
    * feat: hooked up /spots endpoint with the frontend partially
    
    * lint: fixed linting errors
    
    * polish: fix linting and type issue
    
    * lint: Remove mock data since we don't need it anymore
    
    * lint: Remove coalesence since backend PR a fix
    
    ---------
    
    Co-authored-by: TanoVip123 <nguyentano200@gmail.com>
    Co-authored-by: ioreel <ioreel@users.noreply.github.com>
    Co-authored-by: TanoVip123 <42956543+TanoVip123@users.noreply.github.com>

---
+++ webapp/src/lib/components/spot-listings/detail-modal.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+    import { Modal } from 'carbon-components-svelte';
+    export let listing;
+    export let open = false;
+    import { MapLibre, DefaultMarker } from 'svelte-maplibre';
+
+    const defaultZoom: number = 13;
+</script>
+
+<Modal
+    bind:open
+    modalHeading={listing.location.street_address}
+    primaryButtonText="Book now"
+    on:open
+    on:close
+    on:submit
+>
+    <div class="modal-content">
+        <div class="listing-content">
+            <p><strong>Postal Code:</strong> {listing.location.postal_code}</p>
+            <p><strong>City:</strong> {listing.location.city}</p>
+            <p><strong>State:</strong> {listing.location.state}</p>
+            <p><strong>Features:</strong></p>
+            <ul>
+                <li>Charging Station: {listing.features.charging_station ? 'Yes' : 'No'}</li>
+                <li>Plug-In: {listing.features.plug_in ? 'Yes' : 'No'}</li>
+                <li>Shelter: {listing.features.shelter ? 'Yes' : 'No'}</li>
+            </ul>
+        </div>
+        <div>
+            <MapLibre
+                center={[listing.location.longitude, listing.location.latitude]}
+                zoom={defaultZoom}
+                style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+            >
+                <DefaultMarker lngLat={[listing.location.longitude, listing.location.latitude]} />
+            </MapLibre>
+        </div>
+    </div>
+</Modal>
+
+<style>
+    .modal-content {
+        display: flex;
+        flex-direction: row;
+    }
+    .listing-content {
+        padding: 1rem;
+        z-index: 1000;
+        width: 30%;
+        background-color: rgb(209, 209, 209);
+        border: 1px solid black;
+        border-radius: 5px;
+        margin-left: 2rem;
+    }
+</style>
---
+++ webapp/src/lib/components/spot-listings/spots-list-component.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+    import { ChargingStation, PlugFilled, Home } from 'carbon-icons-svelte';
+    import { TooltipIcon } from 'carbon-components-svelte';
+    export let listing;
+</script>
+
+<div class="listing-info" role="button" tabindex="0">
+    <div class="address">
+        <p class="listing-head">{listing.location.street_address}</p>
+        <p>{listing.location.postal_code}</p>
+    </div>
+    <div class="features">
+        <div class="feature">
+            {#if listing.features.charging_station}
+                <TooltipIcon tooltipText="Charging Station Available" direction="right" align="end">
+                    <i><ChargingStation size={20} /></i>
+                </TooltipIcon>
+            {/if}
+        </div>
+        <div class="feature">
+            {#if listing.features.plug_in}
+                <TooltipIcon tooltipText="Plug-in Available" direction="right" align="end">
+                    <i class="icon"><PlugFilled size={20} /></i>
+                </TooltipIcon>
+            {/if}
+        </div>
+        <div class="feature">
+            {#if listing.features.shelter}
+                <TooltipIcon tooltipText="Shelter Available" direction="right" align="end">
+                    <i class="icon"><Home size={20} /></i>
+                </TooltipIcon>
+            {/if}
+        </div>
+    </div>
+</div>
+
+<style>
+    .listing-info {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        padding: 10px;
+        border: 1px solid #ddd;
+        margin: 5px;
+        border-radius: 4px;
+        background-color: #f8f9fa;
+    }
+    .listing-head {
+        font-size: 1.5rem;
+        border-bottom: 2px solid #333;
+        margin-bottom: 5px;
+    }
+    .address {
+        margin-bottom: 10px;
+    }
+    .features {
+        display: flex;
+        flex-direction: row;
+        margin-left: 0.5rem;
+    }
+    .feature {
+        display: flex;
+        align-items: center;
+        font-size: 1rem;
+        margin: 0rem 0.2rem;
+    }
+</style>
