5f1579224140a0c71ec1963003a8f09721f34c36
ioreel <181802858+ioreel@users.noreply.github.com>
2024-12-06 22:21:03 -0600

    fix(webapp): relax CSP for MapLibre GL (#359)
    See https://maplibre.org/maplibre-gl-js/docs/#csp-directives

--- webapp/svelte.config.js
+++ webapp/svelte.config.js
@@ -11,7 +11,7 @@ const config = {
         csp: {
             directives: {
                 'script-src': ['self'],
-                'frame-ancestors': ['none']
+                'worker-src': ['self', 'blob:']
             }
         },
         adapter: adapter({
